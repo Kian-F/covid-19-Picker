94b2db5f3e5f27d79dc88ac19830b30d8fcbd22f
Kian <kian.info78@gmail.com>
2020-04-17 12:32:01 +1000

    chart

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -8,31 +8,49 @@ const Chart =() =>{
 
     // state = {dailyData: {}}
 
-const [dailyData, setDailyData] = useState({});
+const [dailyData, setDailyData] = useState([]);
 
 useEffect(() => {
     const fetchAPI = async () =>{
+
+        // const dailyData = await fetchDailyData();
         setDailyData(await fetchDailyData())
     }
     console.log(dailyData);
-    
+    //call the function
     fetchAPI();
 });
 
 const lineChart = (
-    //if dailyData is availble
- dailyData[0] ?( <Line
+    //if dailyData length is not 0, else return null
+ dailyData.length != 0 ?( <Line
  //we need to make the data dynamic but it is also an object
 data={{
-    labels: '',
-    //
-    datasets: [{},{}],
+
+    //loop trough the dailyData, this is a map and return all the dates
+    labels: dailyData.map(({date}) => date),
+    //an array of objects
+    datasets: [{
+        data: dailyData.map(({confirmed}) => confirmed),
+        label: 'Infected',
+        borderColor: '#3333ff',
+        fill:true, //fill the space below the chart
+    },{
+        data: dailyData.map(({deaths}) => deaths),
+        label: 'Deaths',
+        borderColor: 'black',
+        backgroundColor: 'rgba(255, 0, 0, 0.5)',
+        fill: true,
+
+    }],
 }}
 />) : null
 );
 
     return(
-        <h1>Charts</h1>
+        <div className={styles.container}>
+                {lineChart}
+        </div>
     )
 }
 
